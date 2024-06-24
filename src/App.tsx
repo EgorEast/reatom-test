87e2597da7ab52cb37d218c1c9682d5ec23ef6d8
egoreast <egoreast.true@gmail.com>
2024-06-24 14:44:20 +0300

    removed @reatom/core package from list of project dependencies and updated subscription in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
+import { useEffect } from 'react'
 import { inputAtom, greetingAtom, onSubmit } from './model'
 import { Search } from './Search'
-import { useAtom } from '@reatom/npm-react'
-import { reatomCtx } from './Providers'
+import { useAtom, useCtx } from '@reatom/npm-react'
 import './App.css'
-
-reatomCtx.subscribe(greetingAtom, greeting => {
-  const greetingEl = document.getElementById('greeting')
-
-  if (!greetingEl) return
-
-  greetingEl.innerText = greeting
-})
 
 function App() {
   const [input, setInput] = useAtom(inputAtom)
   const [greeting] = useAtom(ctx => `common:GREETING ${ctx.spy(inputAtom)}!`)
+  const ctx = useCtx()
+
+  useEffect(() => {
+    ctx.subscribe(greetingAtom, greeting => {
+      const greetingEl = document.getElementById('greeting')
+
+      if (!greetingEl) return
+
+      greetingEl.innerText = greeting
+    })
+  }, [])
 
   return (
     <>
@@ -22,7 +25,7 @@ function App() {
       <input id='name' onInput={e => setInput(e.currentTarget.value)} />
       <div id='greeting'></div>
       <div style={{ color: 'red' }}>{greeting}</div>
-      <button onClick={() => onSubmit(reatomCtx)}>save</button>
+      <button onClick={() => onSubmit(ctx)}>save</button>
       <Search />
     </>
   )
